03f6ac1cadbf13d62dd23503d4ba90d64b1572e7
adrienclesse <adrienclesse@hotmail.com>
2021-03-12 14:42:10 +0100

    Adding first text line and linking new css file to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>Language exchange app</h1>
+      <p>Welcome to our website : here, people team up and make activites to learn a language toegether as a community.  People can meet toether to take an online quiz and meet in real live event"</p>
     </div>
   );
 }
