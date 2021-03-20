12e58144ad33b6aecf7e9289584c1dacd348eb01
Selin Tezel <selin.tezel@hotmail.com>
2021-03-20 10:00:46 +0100

    landing page styling WIP

--- client/src/Components/Main/Main.js
+++ client/src/Components/Main/Main.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router,Link, Switch} from 'react-router-dom';
+import Route from 'react-router-dom/Route.js';
+import Login from '../Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import {Card} from "react-bootstrap";
 import "./main.css";
 
 
 
-function clickMe(){
-alert('you clicked me ');
-}
+
+// function clickMe(){
+// alert('you clicked me ');
+// }
 
 
 
@@ -16,23 +20,32 @@ class  Main extends React.Component {
 
     render() {
       return (
+      <Router>
         <div className='main'>  
             <div className='main-container'> 
               <div className='main-body'>
                 <h1 className='main-title'>
-                  We Cheer you Up With Our Scheduled Language Activities.
                 </h1>
                   <div className='main-divtext'>
                     <p className='main-text'>
-                     Welcome to  Speactivity! Here, people team up and make activites to learn a language together.
+                    <span>Discover</span><br/> our language<br/> activities with<br/> <strong> Speacty!</strong>   
                     </p>
+                    <div className="main-divbutton">
+                   <Link className="main-button"to='/Login'>JOIN US</Link>
+                  
                   </div>
-                  <div className="main-divbutton">
-                    <button onClick={clickMe} className='register-button'>Click to register</button>
                   </div>
+                  {/* <p>Here at Speacty anyone can team up and make activites to learn a language together.</p> */}
+                  
               </div>
             </div>
         </div>
+          <Switch>
+              <Route exact strict path='/Login'>
+                <Login/>
+              </Route>
+          </Switch>
+      </Router>
     );
   }
   }
