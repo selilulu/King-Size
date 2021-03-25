37e0c8e463558e6cb6f1f8f16f8171214a71c6da
adrienclesse <adrienclesse@hotmail.com>
2021-03-25 11:39:34 +0100

    Events page styling

--- client/src/Components/Events/Events.js
+++ client/src/Components/Events/Events.js
@@ -31,14 +31,14 @@ function Events() {
 
     <div className='card-container'>
     <div className='image-conatiner'>
-        <img src='/images/logo.png' alt=''></img>
+        <img src='/images/boardgame.png' alt=''></img>
     </div>
     <div className='card-content'>
         <div className='card-title'>
-        <h3> Learn with a language game</h3>
+        <h3> Play a boardgame learning French</h3>
         </div>
         <div className='class-body'>
-        <p> In this online language game, learn with others news words and other expressions meanwhile having fun. </p>
+        <p> TIme to relax and play a boardgame toegether but attention, only French allowed! </p>
         </div>
         <div className='btn'>
             <button>
@@ -53,14 +53,14 @@ function Events() {
 
        <div className='card-container'>
             <div className='image-conatiner'>
-                <img src='/images/logo.png' alt=''></img>
+                <img src='/images/debate.png' alt=''></img>
             </div>
             <div className='card-content'>
                 <div className='card-title'>
-                   <h3> Learn with a language game</h3>
+                   <h3> Debate on a given subject</h3>
                 </div>
                 <div className='class-body'>
-                   <p> In this online language game, learn with others news words and other expressions meanwhile having fun. </p>
+                   <p> The best opportunity to practice a language is to debate about any subject with other learners </p>
                 </div>
                 <div className='btn'>
                     <button>
